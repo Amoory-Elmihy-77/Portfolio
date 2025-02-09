6965531441cb86449b38d1a43081fb3eef4e9232
3moory <amaryasser2358@gmail.com>
2025-02-09 15:21:14 +0200

    upload all portfolio files

---
+++ src/loadData.js
@@ -0,0 +1,92 @@
+// projectData.js
+export const projects = [
+  {
+    id: "1",
+    Title: "ToDo App",
+    Description: "A modern todo app built with React and Material UI",
+    Img: "/portfolio/projects/todo.jpg",
+    Link: "https://todo-elmihy72.netlify.app/",
+    Github: "https://github.com/Amoory-Elmihy-77/Todo.git",
+    TechStack: ["React", "Tailwind", "JavaScript"],
+    Features: [
+      "Responsive design",
+      "Dark mode support",
+      "Project showcase",
+      "Contact form"
+    ]
+  },
+  {
+    id: "2",
+    Title: "Weather App",
+    Description: "A weather app in specific city built with Axios",
+    Img: "/portfolio/projects/weather.jpg",
+    Link: "https://weather-cairo-elmihy74.netlify.app/",
+    Github: "https://github.com/Amoory-Elmihy-77/Weather-App.git",
+    TechStack: ["React", "Axios"],
+    Features: [
+      "Responsive design",
+      "Dark mode support",
+      "Project showcase",
+      "Contact form"
+    ]
+  },
+  {
+    id: "3",
+    Title: "Movflix App",
+    Description: "A movies website with Java Script and Api",
+    Img: "/portfolio/projects/movflix.jpg",
+    Link: "https://movflix-elmihy74.netlify.app/",
+    Github: "https://github.com/Amoory-Elmihy-77/Movflix.git",
+    TechStack: ["Java Script"],
+    Features: [
+      "Responsive design",
+      "Dark mode support",
+      "Project showcase",
+      "Contact form"
+    ]
+  },
+  {
+    id: "4",
+    Title: "E-commerce",
+    Description: "that is an page for cloth shop with js",
+    Img: "/portfolio/projects/ecommerce.jpg",
+    Link: "https://elmihy-ecommerce74.netlify.app/",
+    Github: "https://github.com/Amoory-Elmihy-77/E-commerce.git",
+    TechStack: ["Java Script"],
+    Features: [
+      "Responsive design",
+      "Dark mode support",
+      "Project showcase",
+      "Contact form"
+    ]
+  },
+];
+
+export const certificates = [
+  {
+    id: "1",
+    Img: "/portfolio/certificates/react.jpg",
+    Title: "React Development - Udemy"
+  },
+  {
+    id: "2",
+    Img: "/portfolio/certificates/nti.jpg",
+    Title: "MEAN Stack Development - NTI"
+  },
+  {
+    id: "2",
+    Img: "/portfolio/certificates/gdg.jpg",
+    Title: "Top 10 in GDG Delta Hackthon"
+  },
+];
+
+export const techStacks = [
+  { icon: "html.svg", language: "HTML" },
+  { icon: "css.svg", language: "CSS" },
+  { icon: "javascript.svg", language: "JavaScript" },
+  { icon: "tailwind.svg", language: "Tailwind CSS" },
+  { icon: "reactjs.svg", language: "ReactJS" },
+  { icon: "vite.svg", language: "Vite" },
+  { icon: "bootstrap.svg", language: "Bootstrap" },
+  { icon: "MUI.svg", language: "Material UI" }
+];
